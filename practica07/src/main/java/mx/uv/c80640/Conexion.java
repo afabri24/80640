3c7d233557748ac9af36b6b4c243aa51ef5c6681
afabri24 <afabri24@gmail.com>
2022-11-28 18:57:27 -0600

    Actualizacion

--- practica07/src/main/java/mx/uv/c80640/Conexion.java
+++ practica07/src/main/java/mx/uv/c80640/Conexion.java
@@ -1,22 +1,24 @@
 package mx.uv.c80640;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Connection;
 
 public class Conexion {
-    private static String url= "jdbc:mysql://127.0.0.1:3306/nombreBD";
-    private static String DriverName="com.mysql.cj.jdbc.Driver";
-    private static String username="root";
-    private static String password="";
-    private static Connection connection=null;
+    private static String url = "jdbc:mysql://127.0.0.1:3306/ejemplo80640";
+    private static String DriverName = "com.mysql.jdbc.Driver";
+    private static String username = "root";
+    private static String password = "";
+    private static Connection connection = null;
+
     public static Connection getConnection(){
-        try{
+        try {
             Class.forName(DriverName);
-            connection=DriverManager.getConnection(url,username, password);
-        }catch(SQLException e){
+            connection = (Connection) DriverManager.getConnection(url, username, password);
+            System.out.println("listo conectado");
+        } catch (SQLException e){
             System.out.println(e);
-        }catch(ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
             System.out.println("no encontro el driver");
         }
         return connection;
